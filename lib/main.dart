b416994ebe1d5b86321c5df06363f1864cfcf5ca
IndiraJayanti <165116625+IndiraJayanti@users.noreply.github.com>
2024-10-27 21:27:05 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,149 +1,112 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: '',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 10, 56, 94)),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        body: WebLayout(),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class WebLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
+    return Row(
+      children: [
+        // Sidebar
+        Flexible(
+          flex: 2,
+          child: Container(
+            color: Colors.blueGrey[800],
+            child: Column(
+              children: [
+                DrawerHeader(
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.menu,
+                        color: Colors.white,
+                        size: 28,
+                      ),
+                      SizedBox(width: 10), // Spasi antara icon dan teks
+                      Text(
+                        'Menu',
+                        style: TextStyle(color: Colors.white, fontSize: 24),
+                      ),
+                    ],
+                  ),
+                ),
+                ListTile(
+                  leading: Icon(Icons.home, color: Colors.white),
+                  title: Text('Home', style: TextStyle(color: Colors.white)),
+                  onTap: () {},
+                ),
+                ListTile(
+                  leading: Icon(Icons.settings, color: Colors.white),
+                  title: Text('Settings', style: TextStyle(color: Colors.white)),
+                  onTap: () {},
+                ),
+                ListTile(
+                  leading: Icon(Icons.info, color: Colors.white),
+                  title: Text('About', style: TextStyle(color: Colors.white)),
+                  onTap: () {},
+                ),
+              ],
+            ),
+          ),
+        ),
+        
+        // Main Content
+        Expanded(
+          flex: 8,
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Image.network('https://d22gwcrfo2de51.cloudfront.net/wp-content/uploads/2020/08/logo_Universitas-Pendidikan-Nasional-thumb.png',
-              width: 350,
-              height: 350,
-              ),
-              SizedBox(height: 20, width: 50),
-              TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Email',
-                  prefixIcon: Icon(Icons.email, color: Colors.amber),
+            children: [
+              // Header
+              Container(
+                padding: EdgeInsets.all(16),
+                color: const Color.fromARGB(255, 58, 72, 79),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Header',
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.notifications, color: Colors.white),
+                      onPressed: () {},
+                    ),
+                  ],
                 ),
               ),
-              SizedBox(height: 15, width: 50,),
-              TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Password',
-                  prefixIcon: Icon(Icons.key, color: Colors.amber),
-                ),
-              ),
-              SizedBox(height: 20, width: 50,),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () {
-
-                  },
-                  child: Text('Login'),
-                  style: ElevatedButton.styleFrom(
-                    padding: EdgeInsets.symmetric(vertical: 15),
+              
+              // Content
+              Expanded(
+                child: Container(
+                  color: const Color.fromARGB(255, 214, 212, 212),
+                  child: Center(
+                    child: Text(
+                      'This is Content Area',
+                      style: TextStyle(fontSize: 24),
+                    ),
                   ),
                 ),
               ),
-              SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextButton(
-                    onPressed: (){
-                    }, 
-                    child: Text('Forget Password'),
-                  ),
-                  Text("|"),
-                  TextButton(
-                    onPressed: () {
-                    }, 
-                    child: Text('Register Here'),
-                  ),
-                ],
-              )
             ],
           ),
-          ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        ),
+      ],
     );
   }
 }
